8272cf869dc14e401675785a3b4c3a3564b98f60
Denis Kiselyov <dk.well.done@gmail.com>
2021-08-11 17:22:36 +0300

    add data directory for smooth launch

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run --detach --restart always -p 5432:5432 -v $(pwd)/data:/var/lib/postgresql/data -e POSTGRES_INITDB_ARGS="--locale=ru_RU.UTF8" marinad/postgres
+docker run --detach --restart always -p 5432:5432 -v $(pwd)/data:/var/lib/postgresql/data -e POSTGRES_INITDB_ARGS="--locale=ru_RU.UTF8" -e POSTGRES_HOST_AUTH_METHOD=trust marinad/postgres
 
